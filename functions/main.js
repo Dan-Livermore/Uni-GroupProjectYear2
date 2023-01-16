9ef4d36469d46292fcfc327e0fbf52bf8163249e
Dan-Livermore <91542245+Dan-Livermore@users.noreply.github.com>
2023-01-16 11:16:12 +0000

    Dan trying to connect sessions pages (#19)
    * Merge conflict on index + added header to session1
    
    * header + footer go display images correctly
    
    * Correctly connected headers on all pages

--- functions/main.js
+++ functions/main.js
@@ -4,7 +4,7 @@ class MyHeader extends HTMLElement {
       <nav class="navbar has-shadow is-primary">
        <div class="navbar-brand">
            <a class="navbar-item" href="index.html">
-               <img src="../assets/logo.png" alt="site logo" style="max-height: 70px;" class="py-2 px-2">
+               <img src="/./assets/logo.png" alt="site logo" style="max-height: 70px;" class="py-2 px-2">
            </a>
            <a href="" class="navbar-burger" id="burger">
                <span></span>
@@ -15,13 +15,13 @@ class MyHeader extends HTMLElement {
    
        <div class="navbar-menu" id="nav-links">
            <div class="navbar-start">
-               <a href="about.html" class="navbar-item">About</a>
-               <a href="news.html" class="navbar-item">News</a>
-               <a href="sessions/session1/contents.html" class="navbar-item">Sessions</a>
-               <a href="team.html" class="navbar-item">Team</a>
-               <a href="heallthprofs.html" class="navbar-item">Health Profs</a>
-               <a href="contact.html" class="navbar-item">Contact</a>
-               <a href="furthersupport.html" class="navbar-item">Further Support</a>
+               <a href="/./pages/about.html" class="navbar-item">About</a>
+               <a href="/./pages/news.html" class="navbar-item">News</a>
+               <a href="/./pages/sessions/session1/contents.html" class="navbar-item">Sessions</a>
+               <a href="/./pages/team.html" class="navbar-item">Team</a>
+               <a href="/./pages/heallthprofs.html" class="navbar-item">Health Profs</a>
+               <a href="/./pages/contact.html" class="navbar-item">Contact</a>
+               <a href="/./pages/furthersupport.html" class="navbar-item">Further Support</a>
            </div>  
            <div class="navbar-end">
                <a href="login.html" class="navbar-item">Log In</a>
@@ -40,10 +40,10 @@ class MyHeader extends HTMLElement {
       this.innerHTML = `   
         <footer class="footer has-shadow">
             <div class="content has-text-centered">
-                <img src="../assets/footer.webp" alt="Footer image containing related helpful links">
+                <img src="/./assets/footer.webp" alt="Footer image containing related helpful links">
                 <p>Copyright 2022 Face It | Registered Charity No. 1011222 </p>
-                <a href="terms.html">Terms + Conditions  |  </a>
-                <a href="contact.html">Contact Us</a>
+                <a href="/./pages/terms.html">Terms + Conditions  |  </a>
+                <a href="/./pages/contact.html">Contact Us</a>
                 
                 
             </div>

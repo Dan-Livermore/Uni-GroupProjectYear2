87e2c8a4545ae14df7e3051adf0b2780b5c7de4a
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-02-23 12:34:34 +0000

    Added SQL files in its own folder

---
+++ DB_SQL/Creating Tables/Create Assigned healthcare to user table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE FaceIt.HealthProfUsers (
+    prof_id int NOT NULL,
+    user_id int NOT NULL
+
+    CONSTRAINT AssignedUsers PRIMARY KEY (prof_id, user_id)
+
+    
+
+)
---
+++ DB_SQL/Creating Tables/Create Journal Table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE FaceIt.Journal 
+(user_id int NOT NULL,
+journal_entry VARCHAR(255),
+entry_date VARCHAR(10) NOT NULL
+)

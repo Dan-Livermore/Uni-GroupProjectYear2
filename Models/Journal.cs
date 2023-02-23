269e369ba6cde7dcda1afce3d6664c645da4bda8
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-02-23 13:32:24 +0000

    Scaffolded Database, Not tested if it works yet as no endpoint to test

---
+++ Models/Journal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaceIt2023.Models;
+
+[Keyless]
+[Table("Journal", Schema = "FaceIt")]
+public partial class Journal
+{
+    [Column("user_id")]
+    public int UserId { get; set; }
+
+    [Column("journal_entry")]
+    [StringLength(255)]
+    [Unicode(false)]
+    public string? JournalEntry { get; set; }
+
+    [Column("entry_date")]
+    [StringLength(10)]
+    [Unicode(false)]
+    public string EntryDate { get; set; } = null!;
+}

6b2885e5d239f87c07a96f7513009a7ba301d85f
Dan-Livermore <daniel.livermore@students.plymouth.ac.uk>
2023-02-24 11:23:17 +0000

    Added main.js to all of the main pages

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -6,11 +6,11 @@ const navbarMenu = document.querySelector('#nav-links');
 
   class MyHeader extends HTMLElement {
     connectedCallback() {
-      this.innerHTML = `   
+      this.innerHTML = ` 
       <nav class="navbar has-shadow is-info">
         <div class="navbar-brand">
           <a class="navbar-item" href="index.html">
-            <img src="../images/Logo.png" alt="site logo" style="max-height: 70px;" class="py-2 px-2"></img>
+            <img src="/wwwroot/images/logo.png" alt="site logo" style="max-height: 70px;" class="py-2 px-2"></img>
           </a>
           <a href="" class="navbar-burger" id="burger">
             <span></span>
@@ -23,7 +23,7 @@ const navbarMenu = document.querySelector('#nav-links');
           <div class="navbar-start">
               <a href="/wwwroot/pages/about.html" class="navbar-item">About</a>
               <a href="/wwwroot/pages/news.html" class="navbar-item">News</a>
-              <a href="/wwwroot//pages/sessions/session1/contents.html" class="navbar-item">Sessions</a>
+              <a href="/wwwroot/pages/sessions/session1/contents.html" class="navbar-item">Sessions</a>
               <a href="/wwwroot/pages/team.html" class="navbar-item">Team</a>
               <a href="/wwwroot/pages/heallthprofs.html" class="navbar-item">Health Profs</a>
               <a href="/wwwroot/pages/contact.html" class="navbar-item">Contact</a>
@@ -45,10 +45,10 @@ const navbarMenu = document.querySelector('#nav-links');
       this.innerHTML = `   
         <footer class="footer has-shadow">
             <div class="content has-text-centered">
-                <img src="../images/footer.webp" alt="Footer image containing related helpful links">
+                <img src="/wwwroot/images/footer.webp" alt="Footer image containing related helpful links">
                 <p>Copyright 2022 Face It | Registered Charity No. 1011222 </p>
-                <a href="../pages/terms.html">Terms + Conditions  |  </a>
-                <a href="../pages/contact.html">Contact Us</a>
+                <a href="/wwwroot/pages/terms.html">Terms + Conditions  |  </a>
+                <a href="/wwwroot/pages/contact.html">Contact Us</a>
             </div>
         </footer>
   `;

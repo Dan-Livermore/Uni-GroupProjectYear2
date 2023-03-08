761975d9d8d20bad1bf6e7c079b39cfcd919b040
Lucas Pawley <lucas.pawley@students.plymouth.ac.uk>
2023-03-08 13:06:28 +0000

    Added session 2 pages, updated layout for team, terms, news, healthprofs pages. Finished Session 1 Activity and started to update about page

--- wwwroot/js/sessions/session1/page8activity.js
+++ wwwroot/js/sessions/session1/page8activity.js
@@ -1,5 +1,12 @@
-
 burgerIcon.addEventListener("click", (event) => {
     navbarMenu.classList.toggle("is-active");
     event.preventDefault();
   });
+
+document.getElementById("save1").addEventListener("saved1", onclick_saved);
+
+function onclick_saved(){
+  var answer1 = document.getElementById("session1ActivityAnswer1").value;
+  alert(answer1);
+}
+

990e5c4827069393a9df1a88d7ebaa02d9e826b9
Dan-Livermore <daniel.livermore@students.plymouth.ac.uk>
2023-03-24 12:37:44 +0000

    merge docker

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -24,6 +24,7 @@
               <a href="/wwwroot/lib/heallthprofs.html" class="navbar-item">Health Profs</a>
               <a href="/wwwroot/lib/contact.html" class="navbar-item">Contact</a>
               <a href="/wwwroot/lib/furthersupport.html" class="navbar-item">Further Support</a>
+              <a href="/wwwroot/lib/journal.html" class="navbar-item">Journals</a>
           </div>  
           <div class="navbar-end">
             <a href="/wwwroot/lib/login.html" class="navbar-item">Log In</a>
@@ -43,7 +44,7 @@
             <div class="content has-text-centered">
                 <img src="/wwwroot/images/footer.webp" alt="Footer image containing related helpful links">
                 <p>Copyright 2022 Face It | Registered Charity No. 1011222 </p>
-                <a href="/wwwroot/lib/terms.html">Terms + Conditions  |  </a><a href="/wwwroot/lib/contact.html">Contact Us</a>
+                <a href="/wwwroot/lib/terms.html">Terms + Conditions</a>  |  <a href="/wwwroot/lib/contact.html">Contact Us</a>
                 <p>Developed by Team Z</p>
             </div>
         </footer>

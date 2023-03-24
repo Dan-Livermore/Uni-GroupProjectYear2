400d24bde2c34720e63a25fba8fb93954577b542
Lucas Pawley <lucas.pawley@students.plymouth.ac.uk>
2023-03-24 13:15:39 +0000

    Javascript fixed, local storage added

--- wwwroot/js/sessions/session2/page1.js
+++ wwwroot/js/sessions/session2/page1.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() { 
-
     let cardToggles = document.getElementsByClassName('card-toggle'); 
-
     for (let i = 0; i < cardToggles.length; i++) { 
-
         cardToggles[i].addEventListener('click', e => { 
-
             e.currentTarget.parentElement.parentElement.childNodes[3].classList.toggle('is-hidden'); 
-
         }); 
-
     } 
+});
 
-}); 
+document.addEventListener("DOMContentLoaded", function() {
+    const textarea = document.getElementById("session1ActivityAnswer1Display");
+    textarea.value = localStorage.getItem("session1ActivityAnswer1");
+
+    const textarea2 = document.getElementById("session1ActivityAnswer2Display");
+    textarea2.value = localStorage.getItem("session1ActivityAnswer2");
+
+    const textarea3 = document.getElementById("session1ActivityAnswer3Display");
+    textarea3.value = localStorage.getItem("session1ActivityAnswer3");
+  });

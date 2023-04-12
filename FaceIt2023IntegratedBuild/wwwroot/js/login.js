28c9e439474046f94dbf283624e2236d9756ddc9
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-04-12 12:21:49 +0100

    Logging in now works, the next step is to make account related pages with privilege according to account level

--- FaceIt2023IntegratedBuild/wwwroot/js/login.js
+++ FaceIt2023IntegratedBuild/wwwroot/js/login.js
@@ -3,3 +3,30 @@ burgerIcon.addEventListener("click", (event) => {
     navbarMenu.classList.toggle("is-active");
     event.preventDefault();
   });
+
+function getAccountDetails (id){
+  const apiUrl = "https://localhost:7200/api/Accounts/";  
+
+  fetch(apiUrl + id)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Store the response in the local storage
+      localStorage.setItem('userEmail', data.userEmail);
+      localStorage.setItem('privilegeLevel', data.privilegeLevel);
+      localStorage.setItem('forename', data.forename);
+      localStorage.setItem('surname', data.surname);
+      console.log(`Account with ID ${id} is now in local storage.`);
+    })
+    .catch(error => console.error(error));
+
+
+
+
+
+
+}

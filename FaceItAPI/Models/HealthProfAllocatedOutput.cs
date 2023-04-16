e4930080bbe875b514cf60a79cc5ea4733643f8e
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-04-16 16:38:44 +0100

    Finished Stored Proc on database and API. Now successfully returns all users paired to a health prof

--- FaceItAPI/Models/HealthProfAllocatedOutput.cs
+++ FaceItAPI/Models/HealthProfAllocatedOutput.cs
@@ -7,8 +7,10 @@ namespace FaceItAPI.Models
     public class HealthProfAllocatedOutput
     {
         [Key]
-        [StringLength(100)]
+        [Column("prof_id")]
         public int prof_id { get; set; }
+        
+        [Column("user_id")]
         public int UserId { get; set; }
     }
 }

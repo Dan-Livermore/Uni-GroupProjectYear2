6b7e782fd22bd37d9d73b62196324984dd34bb62
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-04-16 16:11:58 +0100

    Began implementation of new stored proceedure to return all users matched to a health prof. Unfinished. Do not merge

---
+++ FaceItAPI/Controllers/HealthProfAllocatedInputsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FaceItAPI.Models;
+using Microsoft.Data.SqlClient;
+
+namespace FaceItAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthProfAllocatedInputsController : ControllerBase
+    {
+        private readonly Comp2003ZContext _context;
+
+        public HealthProfAllocatedInputsController(Comp2003ZContext context)
+        {
+            _context = context;
+        }
+
+        //// GET: api/HealthProfAllocatedInputs
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<HealthProfAllocatedInput>>> GetHealthProfAllocatedInput()
+        //{
+        //  if (_context.HealthProfAllocatedInput == null)
+        //  {
+        //      return NotFound();
+        //  }
+        //    return await _context.HealthProfAllocatedInput.ToListAsync();
+        //}
+
+        [HttpGet]
+        public IActionResult GetHealthProfAllocatedInput(string hpID)
+        {
+            
+
+            try
+            {
+                SqlParameter idParam = new SqlParameter("@hpID", hpID);
+
+                var result = _context.HealthProfAllocatedOutput
+                    .FromSqlRaw<IdByEmailAndPasswordResult>("EXECUTE FaceIt.get_assigned_users_by_profID @hpID",
+                        idParam)
+                    .ToList();
+
+                if (result == null || result.Count == 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                // log the exception or return an error response
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        private bool HealthProfAllocatedInputExists(int id)
+        {
+            return (_context.HealthProfAllocatedInput?.Any(e => e.prof_id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ FaceItAPI/Models/HealthProfAllocatedOutput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FaceItAPI.Models
+{
+    public class HealthProfAllocatedOutput
+    {
+        [Key]
+        [StringLength(100)]
+        public int prof_id { get; set; }
+        public int UserId { get; set; }
+    }
+}

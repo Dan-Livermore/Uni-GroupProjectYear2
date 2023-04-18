c6517be896f7d9f5b9259af05c2d7ddceb0345e4
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-04-18 11:54:07 +0100

    Save before deleteing and reinstalling packages

---
+++ FaceItAPI/Models/UserAssignedHealthProfInput.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FaceItAPI.Models
+{
+    public class UserAssignedHealthProfInput
+    {
+        [Required]
+        [Key]
+        public int UserId { get; set; }
+    }
+}
---
+++ FaceItAPI/Models/UserAssignedHealthProfOutput.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FaceItAPI.Models
+{
+    public class UserAssignedHealthProfOutput
+    {
+        [Key]
+        public int prof_id { get; set; }
+    }
+}

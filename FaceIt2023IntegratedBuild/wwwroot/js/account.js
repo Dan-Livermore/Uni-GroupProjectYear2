9fe0a30795b73d2fd342fabdc2aff7703ef905b6
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-04-19 12:56:17 +0100

    Added code to get the name and id of assigned health prof by a user with privelege level 3

--- FaceIt2023IntegratedBuild/wwwroot/js/account.js
+++ FaceIt2023IntegratedBuild/wwwroot/js/account.js
@@ -8,6 +8,8 @@ burgerIcon.addEventListener("click", (event) => {
 const privLevel =localStorage.getItem('privilegeLevel');
 const forename = localStorage.getItem('forename');
 const myID = localStorage.getItem('userID');
+const my_Health_prof = 999;
+const my_Health_prof_name = "Name not Found";
 
 //privLevel == 3 means normal user
 //privLevel == 2 means health prof
@@ -32,9 +34,10 @@ function setTitle(privLevel){
 
 //This needs to be edited in future it needs to call the stored procedure to return the health prof allocated to user (i will make this stored proc)
 function getMyHealthProf (myID){
-  const apiUrl = "https://localhost:7200/api/Accounts/";  
+  const apiUrl = "https://localhost:7200/api/UserAssignedHealthProfInputs/";
+  const apiUrl2 = "https://localhost:7200/api/Accounts/"
 
-  fetch(apiUrl + id)
+  fetch(apiUrl + myID)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -42,29 +45,31 @@ function getMyHealthProf (myID){
       return response.json();
     })
     .then(data => {
-      // Store the response in the local storage
-      localStorage.setItem('userEmail', data.userEmail);
-      localStorage.setItem('privilegeLevel', data.privilegeLevel);
-      localStorage.setItem('forename', data.forename);
-      localStorage.setItem('surname', data.surname);
-      console.log(`Account with ID ${id} is now in local storage.`);
-      localStorage.setItem('loggedIn',true);
+      my_Health_prof = data.prof_id;
     })
     .catch(error => console.error(error));
+
+  fetch(apiUrl2+my_Health_prof)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    my_Health_prof_name = data.forename+" "+data.surname;
+  })
+  .catch(error => console.error(error));
 } 
 
 //Need to also add a function to call stored procedure to return all users by healthProf's ID
 
 function setBody(privLevel){
 
+  let card2_Body = document.getElementById("card2Body");
+
   if(privLevel==3){
-    
-    
-
-
-
-
-    const title = "Hello "+forename+" ."
+     //card2_Body. "Hello "+forename+" ."
   
   
   }

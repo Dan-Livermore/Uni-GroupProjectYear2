9c91fc844390f3ca860879c7a213f7bdea4d2462
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-04-24 15:26:05 +0100

    Priv Level 3 correctly displays health prof.

--- FaceIt2023IntegratedBuild/wwwroot/js/account.js
+++ FaceIt2023IntegratedBuild/wwwroot/js/account.js
@@ -11,45 +11,37 @@ var my_forename = localStorage.getItem('forename');
 // var my_Health_prof_name = " <Name not Found> ";
 // var my_Health_prof_email = " <No email address found> ";
 
-function getMyHealthProf2(myID) {
+async function getMyHealthProf2(myID) {
   const apiUrl = "https://localhost:7200/api/UserAssignedHealthProfInputs?ID=";
   const apiUrl2 = "https://localhost:7200/api/Accounts/"
   var my_Health_prof_id = 999;
-
   var my_Health_prof_name = " <Name not Found> ";
   var my_Health_prof_email = " <No email address found> ";
 
+  try {
+    const response1 = await fetch(apiUrl + myID);
+    if (!response1.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data1 = await response1.json();
+    my_Health_prof_id = data1[0].prof_id;
+    console.log("healthprof id is "+my_Health_prof_id);
 
-  fetch(apiUrl + myID)    
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {      
-      //console.log(data);
-      //console.log(apiUrl + myID);
-      my_Health_prof_id = data[0].prof_id;
-      console.log("healthprof id is "+my_Health_prof_id)
-      //console.log(data[0].prof_id);
-      return fetch(apiUrl2 + my_Health_prof_id);
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {      
-      my_Health_prof_name = data.forename + " " + data.surname;
-      my_Health_prof_email = data.user_email;
+    const response2 = await fetch(apiUrl2 + my_Health_prof_id);
+    if (!response2.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data2 = await response2.json();
+    my_Health_prof_name = data2.forename + " " + data2.surname;
+    my_Health_prof_email = data2.userEmail;
+    console.log(data2);
 
-    })
-    .catch(error => console.error(error))
-    .finally(() =>console.log("function: getMyHealhProf2 successfully completed and my_healthprof_name is "+my_Health_prof_name)
-    );
-    return [my_Health_prof_name,my_Health_prof_email];
+    console.log("function: getMyHealhProf2 successfully completed and my_healthprof_name is "+my_Health_prof_name);
+    return { name: my_Health_prof_name, email: my_Health_prof_email };
+  } catch (error) {
+    console.error(error);
+    return { name: my_Health_prof_name, email: my_Health_prof_email };
+  }
 }
 
 function getMyMentees(hpID){
@@ -90,18 +82,16 @@ async function setContent(privLevel){
   const myID = localStorage.getItem('userId');
   const forename = localStorage.getItem('forename');  
 
-  if(privLevel==3){    
+  if (privLevel === 3) {
     console.log("Priv level was determined to be 3");
-    
-     const[str1,str2] = await getMyHealthProf2(myID);
 
-     console.log("now going to change text on html:");
-     document.getElementById("card2Title").innerHTML = "Hi: " + forename + " !";
-      document.getElementById("card2Body").innerHTML = "Your Health Prof is: " + str1 + " You can contact them at: " + str2;
+    const { name, email } = await getMyHealthProf2(myID);
 
-    return;    
-    
-    
+    console.log("now going to change text on html:");
+    document.getElementById("card2Title").innerHTML = "Hi: " + forename + " !";
+    document.getElementById("card2Body").innerHTML = "Your Health Prof is: " + name + " You can contact them at: " + email;
+
+    return;
   }
   else if(privLevel==2){
 

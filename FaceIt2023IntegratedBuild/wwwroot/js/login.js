046c3ee02aa49a191479d86ac30963657d4bfd62
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-04-24 16:16:52 +0100

    login and out now works after testing

--- FaceIt2023IntegratedBuild/wwwroot/js/login.js
+++ FaceIt2023IntegratedBuild/wwwroot/js/login.js
@@ -16,18 +16,17 @@ function getAccountDetails (id){
     })
     .then(data => {
       // Store the response in the local storage
+      console.log("about to set local storage items ");
+      console.log(data);
       localStorage.setItem('userEmail', data.userEmail);
       localStorage.setItem('privilegeLevel', data.privilegeLevel);
       localStorage.setItem('forename', data.forename);
       localStorage.setItem('surname', data.surname);
       console.log(`Account with ID ${id} is now in local storage.`);
       localStorage.setItem('loggedIn',true);
+
+      window.location.href = "account.html";
     })
     .catch(error => console.error(error));
-
-
-
-
-
 
 }
